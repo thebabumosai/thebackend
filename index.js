7bd9c8873ae1b932c1bd9b3401a4d9d6d31223b9
thebabumosai <thebabumosai@gmail.com>
2024-10-05 13:59:32 +0000

    initial

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 const express = require('express')
 const app = express()
 const port = 3000
+require('dotenv').config()
+const ms = require('ms')
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+
+let pujoList = require("./pujos.json")
+function refetchPujoList(){
+  let url = `${process.env.NICO_URL}?offset=0&limit=1000&where=&viewId=vw092rsggz6pkjsy&fields=name,lat,lon,address,city,zone,id,created_at,updated_at`;
+
+    let options = {
+      method: 'GET',
+      headers: {
+        'xc-token': process.env.NICO_TOKEN 
+      }
+    };
+
+    fetch(url, options)
+      .then(res => res.json())
+      .then(json => pujoList = { 
+        result: json.list, 
+        "message": "Pujo list successfully fetched", 
+        "status": "success",
+        "lastRefresh": Date.now()
+      })
+    .catch (err => console.error('error:' + err));
+    return 1
+}
+
+setInterval(()=>refetchPujoList(), ms('2hr'))
+
+app.get('/pujo/list', async (req, res) => {
+  res.json(pujoList)
 })
 
 app.listen(port, () => {
